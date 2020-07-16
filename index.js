80d0eeeffc8ee278f34e8094a67e97c14571c220
kadonomaro <denis.afer@gmail.com>
2020-07-16 21:51:33 +0700

    init: add initial files

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.listen(PORT, () => {
+    console.log('Server has been started on port: ', PORT)
+})
